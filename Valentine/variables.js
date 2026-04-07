9fa29e3ba36bcb42f67da910a56b74c42e20ffc8
Code Emperor <justinemarcfortaleza@gmail.com>
2026-04-07 18:09:43 +0800

    Add files via upload

---
+++ Valentine/variables.js
@@ -0,0 +1,37 @@
+// 4 ways of declaring variables
+// Automatically, var, let, const
+
+
+// automatic variables automatically declare themselves
+// x = 5;
+// y = 12.5;
+// z = "Hello World";
+
+// let - uses the ley keyword, it cannot be redeclared. It is preferred to be used by default when declaring variables
+// example
+// let x = 5;
+// let y = 12.5;
+// let z = "Hello World";
+
+// const - the const keyword is used to declare a constant variable. It cannot be redeclared or reassigned
+// example
+// const x = 5;
+// const y = 12.5;
+// const z = "Hello World";
+
+// var - the var keyword is used to declare a variable. It can be redeclared and reassigned
+// example
+// var x = 5;
+// var y = 12.5;
+// var z = "Hello World";
+
+// data types
+// The data type of data that the varibale is currently holding
+// 1. String - a sequence of characters
+// 2. Number - a number
+// 3. Boolean - true or false
+// 4. Object - a collection of data
+// 5. Array - a collection of data
+// 6. Null - no value
+// 7. Undefined - a variable that has not been assigned a value/no value
+// 8. Symbol - a unique value that's not equal to any other value
